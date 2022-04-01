410036696b61d2bf2ab45e4cc8f52228e56ecd5a
romm2100 <romm2100@mail.ru>
2022-04-01 13:42:10 +0300

    Удаление выбросов

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def import_data():
 
 data = import_data()
 data.drop(['Unnamed: 0'], inplace=True, axis=1)
+
+for x in data:
+    q75, q25 = np.percentile(data.loc[:, x], [75, 25])
+    intr_qr = q75 - q25
+    max = q75 + (1.5 * intr_qr)
+    min = q25 - (1.5 * intr_qr)
+    data.loc[data[x] < min, x] = np.nan
+    data.loc[data[x] > max, x] = np.nan
+
+data.head()
+data = data.dropna(axis = 0)
