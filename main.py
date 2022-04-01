45682e1748ad34776d4970879283e00f24c6a492
romm2100 <romm2100@mail.ru>
2022-04-01 13:34:33 +0300

    create main.py load modules

---
+++ main.py
@@ -0,0 +1,6 @@
+from keras import models
+from keras import layers
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
