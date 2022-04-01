8344c0daec44e080002bbd7a050d23268e2103da
romm2100 <romm2100@mail.ru>
2022-04-01 13:38:12 +0300

    Считывание данных из файла

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+
+
+def import_data():
+    df_bp = []
+    df_bp = pd.read_csv('x_data/X_bp.csv')
+    df_nup = []
+    df_nup = pd.read_csv('x_data/X_nup.csv')
+    df = df_bp.merge(df_nup, left_on='Unnamed: 0', right_on='Unnamed: 0')
+    return df
+
+
+data = import_data()
